879e807a67881301408dc491024babf761cfe969
dishui <onejosefina@gmail.com>
2023-09-22 09:56:20 -0700

    add hextobinary at the crypto-hash function

--- average-work.js
+++ average-work.js
@@ -1,7 +1,9 @@
 const Blockchain = require('./blockchain');
 
+
 const blockchain = new Blockchain();
 blockchain.addBlock({ data: 'initial' });
+console.log('first block', blockchain.chain[blockchain.chain.length-1]);
 let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
 
 const times = [];
@@ -18,7 +20,7 @@ for (let i=0; i<10000; i++) {
     
     average = times.reduce((total, num) => (total + num))/times.length;
     const start =  Date.now();
-    console.log('start', start);
-    console.log(`Time to mine block ${i}: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`);
-    console.log('time it took: ', Date.now() - start);
+   // console.log('start', start);
+   // console.log(`Time to mine block ${i}: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`);
+   // console.log('time it took: ', Date.now() - start);
 }
