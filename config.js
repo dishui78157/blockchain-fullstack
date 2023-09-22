30511fe17806e341a342297cc621400353847f33
dishui <onejosefina@gmail.com>
2023-09-21 17:10:50 -0700

    NEW basic block and chain

---
+++ config.js
@@ -0,0 +1,16 @@
+const MINE_RATE = 1000;
+const INITIAL_DIFFICULTY = 3;
+
+const GENESIS_DATA = {
+    timestamp: 1,
+    lastHash: '-----',
+    hash: 'hash-one',
+    data: [],
+    nonce: 0,
+    difficulty: INITIAL_DIFFICULTY,
+}
+    
+module.exports = { 
+    GENESIS_DATA,
+    MINE_RATE,
+};
